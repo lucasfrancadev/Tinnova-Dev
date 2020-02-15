d7b241c23353646366c84d19246bdd06d888bedb
lucasfrancadev <61083710+lucasfrancadev@users.noreply.github.com>
2020-02-15 10:47:18 -0300

    Update routes.js

--- complementar/backend/src/routes.js
+++ complementar/backend/src/routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const CalculaVotos = require('../../../atividades/Exercise_1/src/controllers/CalculaVotos');
+const Fatorial = require('../../../atividades/Exercise_3/index');
 
 const routes = Router();
 
 routes.post('/Exercise1', CalculaVotos.percentualVotos);
+routes.post('/Exercise3', Fatorial.getFatorial);
 
 module.exports = routes;
