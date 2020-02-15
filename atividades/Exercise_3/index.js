1ebd35dfedbf33f8e0bdc15c4fd86a90a1f56eda
lucasfrancadev <61083710+lucasfrancadev@users.noreply.github.com>
2020-02-15 10:46:29 -0300

    Update index.js

--- atividades/Exercise_3/index.js
+++ atividades/Exercise_3/index.js
@@ -1,9 +1,19 @@
-let number = 6
-let fatorial = number
+module.exports = {
+    async getFatorial(request, response){
+        let {number} = request.body
+        let fatorial = number
 
-while(number > 1){
-    fatorial = fatorial * (number - 1)
-    number = number - 1;
+        while(number > 1){
+            fatorial = fatorial * (number - 1)
+            number = number - 1
+            if (number === 1){
+                try{
+                    console.log(`Fatorial: ${fatorial}`)
+                    return response.json(`Fatorial: ${fatorial}`)
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        }
+    }
 }
-
-console.log(fatorial)
